ebb055a110b458381e1eb56eff6341a91f982066
Kirill Minaev <kv_l@ukr.net>
2018-01-10 12:28:45 +0200

    Registration

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -11,8 +11,21 @@ export default class Registration extends Component{
                 </div>
 
                 <div className='registration-forms'>
+                    <form>
+                        <input type='text' placeholder='Login' className='input-text'/>
+                        <input type='password' placeholder='Password' className='input-text'/>
+                        <p><input type='checkbox' value='remember'/>Remember me</p>
+                        <button type='submit' className='input-button'>Log in</button>
+                    </form>
 
+                    <form>
+                        <input type='email' placeholder='E-mail' className='input-text'/>
+                        <input type='text' placeholder='Login' className='input-text'/>
+                        <input type='password' placeholder='Password' className='input-text'/>
+                        <button type='submit' className='input-button'>Sigh up</button>
+                    </form>
                 </div>
+                <div className='empty'></div>
             </div>
         )
     }
